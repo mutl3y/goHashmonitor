f3bf4b1736c4f686ea0b31c40897fb5c4d80899f
Administrator <admin@example.com>
2019-03-20 21:18:08 +0000

    fleshed out dispatcher select statement started filewriter channel logic

--- hashmonitor/messaging/logger.go
+++ hashmonitor/messaging/logger.go
@@ -1,40 +1,73 @@
 package messaging
 
 import (
-	"github.com/spf13/viper"
+	"fmt"
 	"log"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/spf13/viper"
 )
 
 var pathSep = string(os.PathSeparator)
 
-type logger struct {
-	msg        string
+type msg struct {
+	text       string
+	queue      string
 	mtype      string
 	attachment string
-	color      string
+	priority   int
 	silent     bool
 }
 
-type Loogger interface {
-	ToFile()
+// func (m msg) String() string {
+// 	return fmt.Sprintf("text: \t\t%v\nqueue: \t\t%v\ntype: \t\t%v\nattachment: %v\npriority: \t%v\nsilent: \t%v", m.text, m.queue, m.mtype, m.attachment, m.priority, m.silent)
+// }
+
+type config struct {
+	logDir, logName              string
+	slack, telegram, file, email bool
+}
+
+type logger struct {
+	msgChan chan msg
+	control chan string
+	config  config
+}
+
+// NewLogger  returns a new logger instance
+func NewLogger(c *viper.Viper) *logger {
+	conf := config{}
+	conf.logDir = c.GetString("Core.Log.Dir")
+	conf.logName = c.GetString("Core.Log.File")
+	l := &logger{}
+	l.control = make(chan string, 1)
+	l.msgChan = make(chan msg, 10)
+	return l
+}
+
+// Send a message
+func (l *logger) Send(m msg) error {
+	fmt.Printf("sending  message %v\n", m)
+	l.msgChan <- m
+	return nil
+}
+
+func sendFile(m msg) {
+
 }
 
 func NewLog(c *viper.Viper) *logger {
 	return &logger{}
 }
 
-func toFile(c *viper.Viper) {
-
-	ld := c.GetString("Core.Log.Dir")
-	lf := c.GetString("Core.Log.File")
+func (l *logger) fileWriter(chan msg) {
 	cwd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	dir := strings.Join([]string{cwd, ld}, pathSep)
+	dir := strings.Join([]string{cwd, l.config.logDir}, pathSep)
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
 		err = os.Mkdir(dir, 666)
 		if err != nil {
@@ -42,19 +75,69 @@ func toFile(c *viper.Viper) {
 		}
 
 	}
+	f := &os.File{}
 
-	file := strings.Join([]string{dir, time.Now().Format("2006-01-02_") + lf}, pathSep)
-	// If the file doesn't exist, create it, or append to the file
-	f, err := os.OpenFile(file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	defer f.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	line := time.Now().Format("2006-01-02_15:04:05,") + "\n"
-	if _, err := f.Write([]byte(line)); err != nil {
-		log.Fatal(err)
-	}
+	for {
+		select {
+		case m, ok := <-l.msgChan:
+			if !ok {
+				return
+			}
+			file := strings.Join([]string{dir, time.Now().Format("2006-01-02_") + l.config.logName}, pathSep)
+			if file != f.Name() {
+				// file.close
+				if err := f.Close(); err != nil {
+					fmt.Printf("failed closing file %v %v", f.Name(), err)
+				}
+			}
 
+			// If the file doesn't exist, create it, or append to the file
+			f, err := os.OpenFile(file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+			line := time.Now().Format("2006-01-02_15:04:05,") + m.text
+			if _, err := f.Write([]byte(line)); err != nil {
+				log.Fatal(err)
+			}
+		}
+		// /	defer f.Close()
+	}
 }
 
-func dispatcher() {}
+func (l *logger) dispatcher() {
+	fmt.Printf("Starting dispatcher\n")
+	l.config.file = true
+	for {
+		select {
+		case m, ok := <-l.msgChan:
+			if !ok {
+				fmt.Printf("no more messages to dispatch\n")
+				return
+			}
+			fmt.Printf("recieved message %v\n", m)
+			if l.config.slack {
+				fmt.Printf("sending to slack\n")
+				// 			toSlack(msg)
+			}
+			if l.config.file {
+				fmt.Printf("sending to file\n")
+				// 			toFile(msg)
+			}
+			if l.config.telegram {
+				fmt.Printf("sending to telegram\n")
+				// 			toTelegram(msg)
+			}
+			if l.config.email {
+				fmt.Printf("sending to email\n")
+				// 			toEmail()
+			}
+		case <-l.control:
+			fmt.Printf("Recieved stop signal, exiting in 5 seconds\n")
+			// l.msgChan = nil
+			time.Sleep(5 * time.Second)
+			return
+		}
+	}
+}
--- hashmonitor/messaging/logger_test.go
+++ hashmonitor/messaging/logger_test.go
@@ -2,27 +2,56 @@ package messaging
 
 import (
 	"testing"
+	"time"
 
 	"github.com/spf13/viper"
 )
 
-func Test_toFile(t *testing.T) {
+//
+// func Test_logger_fileWriter(t *testing.T) {
+// 	type args struct {
+// 		c *viper.Viper
+// 	}
+// 	tests := []struct {
+// 		name string
+// 		c    *viper.Viper
+// 	}{
+// 		{"no config", c},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			l := NewLogger()
+// 			l.fileWriter(tt.c)
+// 			toFile(tt.c)
+// 		})
+// 	}
+// }
+
+func TestLogger_Send(t *testing.T) {
 	c := viper.New()
 	c.SetDefault("Core.Log.Dir", "logs")
 	c.SetDefault("Core.Log.File", "hashmonitor.log")
 	c.SetDefault("Core.Log.Rotate", true)
-	type args struct {
-		c *viper.Viper
+
+	l := NewLogger(c)
+
+	go l.dispatcher()
+	time.Sleep(time.Second * 1)
+	defer close(l.control)
+	m := msg{
+		text:  "test message",
+		queue: "device",
+		mtype: "error",
+		// 	attachment: "this is a test \n\t\tattachment \nstring \nit contains many lines\nhow's it looking",
+		priority: 1,
+		silent:   false,
 	}
-	tests := []struct {
-		name string
-		c    *viper.Viper
-	}{
-		{"no config", c},
+
+	for x := 0; x <= 10; x++ {
+		if err := l.Send(m); err != nil {
+			t.Errorf("Failed to send message")
+		}
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			toFile(tt.c)
-		})
-	}
+
+	time.Sleep(2 * time.Second)
 }
